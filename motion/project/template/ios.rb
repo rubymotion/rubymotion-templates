1b5879cb81b5b185bddb5edb2df4fcacaa2992a6
Søren Skovsbøll <soren@wero.es>
2018-10-02 22:01:20 +0200

    fix #14 rake device fails for iPhone XS due to new upper case device id

--- motion/project/template/ios.rb
+++ motion/project/template/ios.rb
@@ -278,8 +278,8 @@ $deployed_app_path = nil
 desc "Deploy on the device"
 task :device => :archive do
   App.info 'Deploy', App.config.archive
-  device_id = (ENV['id'] or App.config.device_id).downcase
-  unless App.config.provisions_all_devices? || App.config.provisioned_devices.include?(device_id)
+  device_id = ENV['id'] || App.config.device_id
+  unless App.config.provisions_all_devices? || (App.config.provisioned_devices & [device_id, device_id.downcase]).any?
     App.fail "Device ID `#{device_id}' not provisioned in profile `#{App.config.provisioning_profile}'"
   end
   env = "XCODE_DIR=\"#{App.config.xcode_dir}\""
