e1329fac8eb3c2124a07d53ea32ac4cc65c625c5
ediathome <edi@ediathome.de>
2019-07-30 11:09:14 +0200

    add/modify config options

--- motion/project/template/osx/config.rb
+++ motion/project/template/osx/config.rb
@@ -33,7 +33,7 @@ module Motion; module Project
     variable :icon, :copyright, :category,
              :embedded_frameworks, :external_frameworks,
              :codesign_for_development, :codesign_for_release,
-             :eval_support, :developer_id
+             :eval_support, :developer_userid, :altool_keychain_item
 
     def initialize(project_dir, build_mode)
       super
